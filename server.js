e996dee8ffcaeb6d1e2b35fdc54cac39a3f84337
Ashiqur Rahman Zeeshan <zeeshan@Zeeshans-iMac.local>
2022-12-31 01:02:19 +0600

    localhost origin add

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const morgan = require('morgan')
 const cors = require("cors")
 
 const corsOptions = {
-  origin: "*",
+  origin: "http://localhost:5173",
   credentials: true, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
