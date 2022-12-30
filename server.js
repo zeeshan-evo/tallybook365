3b57d86916e6dbcff57ce2a396133c3f3a79c27b
Ashiqur Rahman Zeeshan <zeeshan@Zeeshans-iMac.local>
2022-12-31 01:06:35 +0600

    credentials: false

--- server.js
+++ server.js
@@ -15,8 +15,8 @@ const morgan = require('morgan')
 const cors = require("cors")
 
 const corsOptions = {
-  origin: "http://localhost:5173",
-  credentials: true, //access-control-allow-credentials:true
+  origin: true,
+  credentials: false, //access-control-allow-credentials:true
   optionSuccessStatus: 200,
 }
 
