91f4f95dfd8d042ca0825de7ba95e72d1f4bbc30
Ashiqur Rahman Zeeshan <zeeshan@Zeeshans-iMac.local>
2022-12-31 01:29:40 +0600

    httpOnly cookie disable

--- utils/cookies.js
+++ utils/cookies.js
@@ -1,6 +1,6 @@
 async function attachCookies(res, token) {
-  const sevenday = 1000 * 60 *60 *24 * 7
-  res.cookie("authToken", token, {httpOnly:true, expires: new Date(Date.now() + sevenday)})
+  const sevenday = 1000 * 60 * 60 * 24 * 7
+  res.cookie("authToken", token, { expires: new Date(Date.now() + sevenday) }) // httpOnly:true,
 }
 
 module.exports = attachCookies
