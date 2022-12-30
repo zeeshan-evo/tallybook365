cd1747318d6834c2980504e52d81faf5e74763af
Ashiqur Rahman Zeeshan <zeeshan@Zeeshans-iMac.local>
2022-12-31 00:48:56 +0600

    cors options edit

--- server.js
+++ server.js
@@ -14,8 +14,14 @@ const authenticateUser = require('./utils/authorize-authenticate')
 const morgan = require('morgan')
 const cors = require("cors")
 
+const corsOptions = {
+  origin: true,
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+}
 
-app.use(cors())
+
+app.use(cors(corsOptions))
 app.use(morgan('dev'))
 app.use(express.json())
 app.use(cookieParser())
